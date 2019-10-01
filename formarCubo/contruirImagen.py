fe0fa84e453c113f14d015b3375c0068f1b9ae32
lucia_agarcia <lucia.alfonso@alu.uclm.es>
2019-10-01 19:44:40 +0200

    Imagen cubo completa
    La imagen ya funciona correctamente. Faltaría unirlo con el resto del código.

--- formarCubo/contruirImagen.py
+++ formarCubo/contruirImagen.py
@@ -5,50 +5,26 @@
 
 from PIL import Image
 
-img=Image.new('RGBA', (800,400), "black")
-img.save('./formarCubo/background.png')
-img_back= Image.open('./formarCubo/background.png')
+dimension = 3 #Dimension del cubo NxNxN
+padding = 20 #Tamaño de 'baldosa' y de separación
 
-img = Image.new('RGBA', (20, 20), "red") 
-img.save('./formarCubo/red.png')
-img_red= Image.open('./formarCubo/red.png')
+def crearFondo(dimension, padding):
+    ancho = padding * dimension * 5
+    largo = padding * dimension * 4
+    img=Image.new('RGBA', (ancho,largo), (0,0,0,0))
+    img.save('./formarCubo/background.png')
 
-img = Image.new('RGBA', (20, 20), "blue") 
-img.save('./formarCubo/blue.png')
-img_blue= Image.open('./formarCubo/blue.png')
-
-img = Image.new('RGBA', (20, 20), "white") 
-img.save('./formarCubo/white.png')
-img_white= Image.open('./formarCubo/white.png')
-
-img = Image.new('RGBA', (20, 20), "green") 
-img.save('./formarCubo/green.png')
-img_green= Image.open('./formarCubo/green.png')
-
-img = Image.new('RGBA', (20, 20), "orange") 
-img.save('./formarCubo/orange.png')
-img_orange= Image.open('./formarCubo/orange.png')
-
-img = Image.new('RGBA', (20, 20), "yellow") 
-img.save('./formarCubo/yellow.png')
-img_yellow= Image.open('./formarCubo/yellow.png')
-
-
-img= Image.open('./formarCubo/background.png')
-img_red = Image.open('./formarCubo/red.png')
+#Abrir imagenes para formar el cubo
+img = None
+img_yellow = Image.open('./formarCubo/yellow.png')
+img_orange = Image.open('./formarCubo/orange.png')
+img_green = Image.open('./formarCubo/green.png')
+img_white = Image.open('./formarCubo/white.png')
 img_blue = Image.open('./formarCubo/blue.png')
+img_red = Image.open('./formarCubo/red.png')
 
-# ##(Vertical, Horizontal)
-dimension=3
-cubesize = (dimension*20)
-posY = 0
-posX = cubesize +50 
-cont = 0
-posInicial= posX
-piezascara= dimension*dimension
-id='113412431243356312154351123465621234531421541223123456'
 
-def switch_caracter(caracter):
+def switch_caracter(caracter, posX, posY):
     if caracter == '1': img.paste(img_blue, (posX,posY))
     if caracter == '2': img.paste(img_yellow, (posX,posY))
     if caracter == '3': img.paste(img_green, (posX,posY))
@@ -56,27 +32,40 @@ def switch_caracter(caracter):
     if caracter == '5': img.paste(img_white, (posX,posY))
     if caracter == '6': img.paste(img_red, (posX,posY))
 
-for caracter in id:
-    switch_caracter(caracter)
-    posX = posX+21
-    cont = cont+1
-    if (cont%dimension) == 0:
-        posX = posInicial
-        posY= posY + 21
-    if cont == piezascara:
-        posX = posInicial
-        posY = posY + (cubesize*2)
-    if cont == (piezascara*2):
-        posX = 0
-        posY = cubesize+30
-        posInicial = posX
+def construirCubo(dimension, padding):
+    cubesize = (dimension*padding)
+    cont = 0
+    posY = 0
+    posX = cubesize + padding
+    posInicial= posX
+    piezascara= dimension*dimension
+    id='113412431243356312154351123465621234531421541223123456'
 
-    #!Este elif falla.
-    elif (cont%piezascara) == 0:
-        posX = posX + cubesize +20
-        posY = posY-cubesize
-        posInicial = posX
+    for caracter in id:
+        switch_caracter(caracter, posX, posY)
+        posX = posX+padding
+        cont = cont+1
+        if (cont%dimension) == 0:
+            posX = posInicial
+            posY= posY + padding
+        if cont == piezascara:
+            posX = posInicial
+            posY = posY + (cubesize*2)
 
-img.save('./formarCubo/CuboFormado.png')
+        elif cont == (piezascara*2):
+            posX = 0
+            posY = cubesize + (int)(cubesize/2)
+            posInicial = posX
+
+        elif (cont%piezascara) == 0:
+            posX = posX + cubesize + padding
+            posY = posY-cubesize
+            posInicial = posX
+
+    img.save('./formarCubo/CuboFormado.png')
     
+if __name__ == "__main__":
+    crearFondo(dimension, padding)
+    img = Image.open('./formarCubo/background.png')
+    construirCubo(dimension, padding)
 
