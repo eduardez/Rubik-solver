930c4ebb1e929fe8b5b4bdad9eab562dd29077cc
lucia_agarcia <lucia.alfonso@alu.uclm.es>
2019-10-16 16:48:58 +0200

    NodoArbol

--- Dominio/nodoArbol.py
+++ Dominio/nodoArbol.py
@@ -1,30 +1,42 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import random
+import Dominio.Cubo
 class nodoArbol:
 
-    def __init__(self):
+    def __init__(self, cubo):
         self.estado = cubo
         self.coste = 1
         self.accion = ''
         self.profundidad = 0
-        self.f = getF(self)
-
-    def getF(self, algoritmo):
-        if algoritmo == 'anchura':
-            self.f=self.coste
-            pass
-        elif algoritmo == 'profundidad':
-            self.f = 1/(self.profundidad+1)
-            pass
-        elif algoritmo == 'estrella':
-            pass
-        elif algoritmo == 'coste':
-            self.f = self.profundidad
-            pass
-        return f
+        self.f = random.randint()
+       # self.f = self.getF()
 
 
+    # def getF(self, algoritmo):
+    #     if algoritmo == 'anchura':
+    #         self.f=self.coste
+    #         pass
+    #     elif algoritmo == 'profundidad':
+    #         self.f = 1/(self.profundidad+1)
+    #         pass
+    #     elif algoritmo == 'estrella':
+    #         self.f = self.coste + self.heuristica
+    #         pass
+    #     elif algoritmo == 'coste':
+    #         self.f = self.profundidad
+    #         pass
+    #     return f
+
+    def calcularHeuristica(self):
+        pass
+
+    def provocarExcepcion(self, cubo):
+        while(1):
+            nodoArbol(self,cubo)
+
+ 
 
 
     
